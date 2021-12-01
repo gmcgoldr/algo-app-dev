bf667e59f50747fa017f6796e252c334a7861043
gmcgoldr <garrin.mcgoldrick@gmail.com>
2021-11-30 21:29:36 -0500

    add transactions utils

---
+++ src/pyteal_utils/transactions.py
@@ -0,0 +1,136 @@
+from typing import Dict, List, NamedTuple, Tuple
+
+import algosdk as ag
+from algosdk.future import transaction
+from algosdk.future.transaction import PaymentTxn
+from algosdk.kmd import KMDClient
+from algosdk.v2client.algod import AlgodClient
+
+from .clients import get_wallet_handle, get_wallet_id
+
+
+class AccountInfo(NamedTuple):
+    key: str
+    address: str
+
+
+def get_confirmed_transactions(
+    client: AlgodClient, transaction_ids: List[int], timeout_blocks: int
+) -> Dict:
+    """
+    Wait for the network to confirm some transactions and return their info.
+
+    Args:
+        client: the client
+        transaction_ids: list of transactions for which to retreive info
+        timeout_blocks: wait for this many blocks to confirm the transactions
+
+    Returns:
+        list of transaction info for confirmed transactions
+    """
+    start_round = client.status()["last-round"] + 1
+    current_round = start_round
+
+    # the transaction ids that are not yet confirmed
+    waiting_ids = set(transaction_ids)
+    # list of transaction info for confirmed transactions
+    infos = []
+
+    while current_round < start_round + timeout_blocks:
+        for transaction_id in list(waiting_ids):
+            # NOTE: documentation suggests that transactions are "remembered"
+            # by a node for some time after confirmation, but doesn't specify
+            # how long
+            pending_txn = client.pending_transaction_info(transaction_id)
+            if pending_txn["pool-error"]:
+                waiting_ids.remove(transaction_id)
+            elif pending_txn.get("confirmed-round", 0) > 0:
+                infos.append(pending_txn)
+                waiting_ids.remove(transaction_id)
+        if not waiting_ids:
+            break
+        # wait until the end of this block
+        client.status_after_block(current_round)
+        current_round += 1
+
+    return infos
+
+
+def get_confirmed_transaction(
+    client: AlgodClient, transaction_id: int, timeout_blocks: int
+) -> Dict:
+    """
+    See `get_confirmed_transactions`, but for a single transaction id.
+    """
+    confirmed = get_confirmed_transactions(client, [transaction_id], timeout_blocks)
+    if confirmed:
+        return confirmed[0]
+    else:
+        return None
+
+
+def fund_from_genesis(
+    algod_client: AlgodClient, kmd_client: KMDClient, amount: int
+) -> Tuple[AccountInfo, str]:
+    """
+    Create a new account and fund it from the account that received the gensis
+    funds.
+
+    Expects an unencrypted wallet "unencrypted-default-wallet" whose first key
+    is the address of the account with the genesis funds.
+
+    Args:
+        algod_client: client to send node commands to
+        kmd_client: client to use in signing the transaction
+        amount: the quantity of microAlgos to fund
+
+    Returns:
+        the funded account info, and funding transaction id
+    """
+    wallet_id = get_wallet_id(kmd_client, "unencrypted-default-wallet")
+
+    # Get the address of the source account with all the genesis tokens
+    with get_wallet_handle(kmd_client, wallet_id, "") as handle:
+        keys = kmd_client.list_keys(handle)
+        if not keys:
+            raise RuntimeError("funded account not found in wallet")
+        sender_address = keys[0]
+
+    key, address = ag.account.generate_account()
+
+    # Transfer algos to the escrow account
+    params = algod_client.suggested_params()
+    params.fee = 0  # use the minimum network fee
+    txn = PaymentTxn(sender=sender_address, sp=params, receiver=address, amt=amount)
+    # Sign with the sender account keys, managed by its wallet
+    with get_wallet_handle(kmd_client, wallet_id, "") as handle:
+        txn = kmd_client.sign_transaction(handle, "", txn)
+    txid = algod_client.send_transaction(txn)
+
+    return AccountInfo(key, address), txid
+
+
+def group_txns(*txns: transaction.Transaction) -> List[transaction.Transaction]:
+    """
+    Group multiple transactions.
+
+    Args:
+        txns: the transactions to group
+
+    Returns:
+        list of transactions, with the `group` memmber set
+    """
+    gid = transaction.calculate_group_id(txns)
+    for txn in txns:
+        txn.group = gid
+    return txns
+
+
+def pad_lease_bytes(lease: bytes) -> bytes:
+    """
+    Given a string of bytes to use as a lease, right pad with 0s to get the
+    correct number of bytes.
+    """
+    lease = lease[: ag.constants.LEASE_LENGTH]
+    lease = lease + (b"\x00" * max(0, ag.constants.LEASE_LENGTH - len(lease)))
+    return lease
---
+++ tests/test_transactions.py
@@ -0,0 +1,81 @@
+import algosdk as ag
+from algosdk.future.transaction import PaymentTxn
+from algosdk.kmd import KMDClient
+from algosdk.v2client.algod import AlgodClient
+
+from pyteal_utils import transactions
+
+
+def test_fund_from_genesis_funds_new_account(
+    algod_client: AlgodClient, kmd_client: KMDClient
+):
+    account, txid = transactions.fund_from_genesis(
+        algod_client, kmd_client, ag.util.algos_to_microalgos(1000)
+    )
+    _ = transactions.get_confirmed_transaction(algod_client, txid, 5)
+    assert algod_client.account_info(account.address).get(
+        "amount"
+    ) == ag.util.algos_to_microalgos(1000)
+
+
+def test_get_confirmed_transaction_returns_info(
+    algod_client: AlgodClient, kmd_client: KMDClient
+):
+    account1, txid = transactions.fund_from_genesis(
+        algod_client, kmd_client, ag.util.algos_to_microalgos(1000)
+    )
+    _ = transactions.get_confirmed_transaction(algod_client, txid, 5)
+    account2 = transactions.AccountInfo(*ag.account.generate_account())
+
+    params = algod_client.suggested_params()
+
+    txn = PaymentTxn(
+        sender=account1.address,
+        sp=params,
+        receiver=account2.address,
+        amt=ag.util.algos_to_microalgos(1),
+    )
+    txn = txn.sign(account1.key)
+    txid = algod_client.send_transaction(txn)
+
+    info = transactions.get_confirmed_transaction(algod_client, txid, 5)
+    assert info.get("confirmed-round")
+    assert not info.get("pool-error")
+    assert info.get("txn", {}).get("txn", {}).get(
+        "amt", None
+    ) == ag.util.algos_to_microalgos(1)
+
+
+def test_groups_transactions(algod_client: AlgodClient):
+    account1 = transactions.AccountInfo(*ag.account.generate_account())
+    account2 = transactions.AccountInfo(*ag.account.generate_account())
+
+    params = algod_client.suggested_params()
+
+    txn1 = PaymentTxn(
+        sender=account1.address,
+        sp=params,
+        receiver=account2.address,
+        amt=ag.util.algos_to_microalgos(1),
+    )
+    txn2 = PaymentTxn(
+        sender=account2.address,
+        sp=params,
+        receiver=account1.address,
+        amt=ag.util.algos_to_microalgos(1),
+    )
+
+    txns = transactions.group_txns(txn1, txn2)
+
+    assert txns[0].group is not None
+    assert txns[0].group == txns[1].group
+
+
+def test_pad_lease_bytes_pads():
+    n = ag.constants.LEASE_LENGTH
+    assert transactions.pad_lease_bytes(b"ab") == b"ab" + (b"\x00" * (n - 2))
+
+
+def test_pad_lease_bytes_truncates():
+    n = ag.constants.LEASE_LENGTH
+    assert transactions.pad_lease_bytes(b"a" * (n + 1)) == b"a" * n
