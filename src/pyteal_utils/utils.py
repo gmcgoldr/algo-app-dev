b21295b4a2b86344dbabc9fdf366408b88dcd5e9
gmcgoldr <garrin.mcgoldrick@gmail.com>
2021-12-04 19:24:16 -0500

    add key encoding

--- src/pyteal_utils/utils.py
+++ src/pyteal_utils/utils.py
@@ -2,6 +2,7 @@ import base64
 from typing import Dict, NamedTuple, Union
 
 import algosdk as ag
+from algosdk.v2client.models.teal_key_value import TealKeyValue
 from algosdk.v2client.models.teal_value import TealValue
 
 ZERO_ADDRESS = ag.encoding.encode_address(bytes(32))
@@ -49,3 +50,8 @@ def to_value(value: Union[int, bytes]) -> TealValue:
         return TealValue(type=2, uint=value)
     else:
         raise PyTealUtilsError(f"invalid value type: {type(value)}")
+
+
+def to_key_value(key: bytes, value: Union[int, bytes]) -> TealKeyValue:
+    key = base64.b64encode(key).decode("ascii")
+    return TealKeyValue(key, to_value(value))
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,4 @@
+from algosdk.v2client.models.teal_key_value import TealKeyValue
 from algosdk.v2client.models.teal_value import TealValue
 
 from pyteal_utils import utils
@@ -18,3 +19,9 @@ def test_from_value_decodes_values():
 def test_to_value_encodes_values():
     assert utils.to_value(b"a") == TealValue(type=1, bytes="YQ==", uint=None)
     assert utils.to_value(1) == TealValue(type=2, bytes=None, uint=1)
+
+
+def test_to_key_value_encodes_key():
+    assert utils.to_key_value(b"a", 1) == TealKeyValue(
+        key="YQ==", value=TealValue(type=2, bytes=None, uint=1)
+    )
